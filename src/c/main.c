0de010b68fc4f7d7ff861df12e6c06130bb6b3c5
Baptiste Candellier <baptiste@candellier.me>
2026-04-01 20:05:15 +0200

    chore: reformat code, setup desktop env

--- src/c/main.c
+++ src/c/main.c
@@ -8,15 +8,15 @@ static BitmapLayer *s_bmp_mount_layer;
 static GFont s_font_time;
 static GFont s_font_body;
 
-static void update_time() {
+static void update_time()
+{
   // Get a tm structure
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
   // Write the current hours and minutes into a buffer
   static char s_time_buffer[8];
-  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ?
-                                                    "%H:%M" : "%I:%M", tick_time);
+  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_time_buffer);
@@ -29,18 +29,20 @@ static void update_time() {
   text_layer_set_text(s_date_layer, s_date_buffer);
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
   update_time();
 }
 
-static void main_window_load(Window *window) {
+static void main_window_load(Window *window)
+{
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   s_font_time = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_RENOGARE_42));
   s_font_body = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_RENOGARE_24));
-  
+
   // Create the time TextLayer
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -62,14 +64,15 @@ static void main_window_load(Window *window) {
   s_bmp_mount_layer = bitmap_layer_create(GRect(5, 5, bounds.size.w, bounds.size.h));
   bitmap_layer_set_compositing_mode(s_bmp_mount_layer, GCompOpSet);
   bitmap_layer_set_bitmap(s_bmp_mount_layer, s_bmp_mount);
-  
+
   // Add layers to the Window
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bmp_mount_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
-static void main_window_unload(Window *window) {
+static void main_window_unload(Window *window)
+{
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
   gbitmap_destroy(s_bmp_mount);
@@ -78,18 +81,18 @@ static void main_window_unload(Window *window) {
   fonts_unload_custom_font(s_font_body);
 }
 
-static void init() {
+static void init()
+{
   // Create main Window element and assign to pointer
   s_main_window = window_create();
-  
+
   // Set the background color
   window_set_background_color(s_main_window, GColorBlack);
 
   // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
+  window_set_window_handlers(s_main_window, (WindowHandlers){
+                                                .load = main_window_load,
+                                                .unload = main_window_unload});
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
@@ -101,12 +104,14 @@ static void init() {
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
-static void deinit() {
+static void deinit()
+{
   // Destroy Window
   window_destroy(s_main_window);
 }
 
-int main(void) {
+int main(void)
+{
   init();
   app_event_loop();
   deinit();
